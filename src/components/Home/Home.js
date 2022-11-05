80f9f1217f1c012a9869b283d14ce678e248ee33
RUHANI77 <ruhaniakter1234@gmail.com>
2022-11-05 14:46:53 +0600

    added header image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import homeImage from './quition.jpg';
 
 const Home = () => {
     return (
-        <div>
+        <div className='grid md:grid-cols-2 sm:grid-cols-1 m-10 p-5 gap-5'>
+            <div>
+                <h4 className='m-4 p-8 bg-slate-200 rounded-md font-bold'>WELCOME TO QUIZZARD!</h4>
+                <p className='text-2xl text-amber-400 font-semibold'>Test your skills!</p>
+                <p>This is a platform where you can test your knowledge in various web development languages.</p>
+            </div>
+
+            <div>
+                <img className='bg-auto bg-no-repeat bg-center' src={homeImage} alt='home background images'></img>
+            </div>
            
-            <h4>This is header</h4>
         </div>
     );
 };
